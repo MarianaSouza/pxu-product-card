1869a9ba31269c7c77dd8093ff67556f135e2d6e
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2021-01-24 14:47:20 -0800

    adding JSON to render assets, adding styles to Products

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,21 +1,23 @@
 import React from "react";
+import './product.scss';
 
-function Product({image = 'https://via.placeholder.com/200X400', alt, description, price, rating}) {
+function Product({ image, alt, description, price, rating }) {
   return (
-
-		<div className="card">
-			<div className="card-body">
-				<img src={image} alt={alt} className="img-fluid"/>
-				<p className="card-text">
-                    {description}
-				</p>
-				<p>
-					{price}
-				</p>
-				<div>{rating}</div>
-				<button className="btn">Add to cart</button>
-			</div>
-		</div>
+    <div className="col-md-6 no-gutters">
+      <div className="card">
+        <div className="card-body">
+          <div className="product-selection">
+            <img src={image} alt={alt} className="img-fluid" />
+            <button className="btn btn-primary add-cart" onClick={()=> alert("Product added to cart")}>Add to cart</button>
+          </div>
+          <div className="product-content">
+            <p className="card-text">{description}</p>
+            <p>{price}</p>
+            <div>{rating}</div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,17 +1,15 @@
 import React from "react";
 import Product from "../Product";
+import products from "./products.json";
 import "./productList.scss";
 
 function ProductList() {
   return (
     <div className="container-fluid list-wrapper">
       <div className="row">
-        <div className="col-md-6">
-          <Product />
-        </div>
-        <div className="col-md-6">
-          <Product />
-        </div>
+          {products.map((product) => {
+            return <Product key={product.id} image={product.image} alt={product.alt} description={product.description} price={product.price} product={product.rating} />;
+          })}
       </div>
     </div>
   );
